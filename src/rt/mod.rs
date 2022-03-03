e107377803fe8144592d834cf41b4204cb9f2df3
zuon <zuon@yinengyun.com>
2022-03-03 15:08:32 +0800

    更新了minstant库的版本，并修改相应代码

--- src/rt/mod.rs
+++ src/rt/mod.rs
@@ -982,7 +982,7 @@ impl<O: Default + 'static, P: AsyncTaskPoolExt<O> + AsyncTaskPool<O>> AsyncTaskT
         AsyncTaskTimer {
             producor,
             consumer,
-            timer: Arc::new(RefCell::new(LocalTimer::<AsyncTimingTask<O, P>, 1000, 60, 60, 24>::new(1, (minstant::now() as f64 * minstant::nanos_per_cycle() / 1000000.0).trunc() as u64))),
+            timer: Arc::new(RefCell::new(LocalTimer::<AsyncTimingTask<O, P>, 1000, 60, 60, 24>::new(1, minstant::Instant::now().elapsed().as_millis() as u64))),
         }
     }
 
@@ -992,7 +992,7 @@ impl<O: Default + 'static, P: AsyncTaskPoolExt<O> + AsyncTaskPool<O>> AsyncTaskT
         AsyncTaskTimer {
             producor,
             consumer,
-            timer: Arc::new(RefCell::new(LocalTimer::<AsyncTimingTask<O, P>, 1000, 60, 60, 24>::new(time as u64, (minstant::now() as f64 * minstant::nanos_per_cycle() / 1000000.0).trunc() as u64))),
+            timer: Arc::new(RefCell::new(LocalTimer::<AsyncTimingTask<O, P>, 1000, 60, 60, 24>::new(time as u64, minstant::Instant::now().elapsed().as_millis() as u64))),
         }
     }
 
@@ -1036,7 +1036,7 @@ impl<O: Default + 'static, P: AsyncTaskPoolExt<O> + AsyncTaskPool<O>> AsyncTaskT
 
     /// 判断当前时间是否有可以弹出的任务，如果有可以弹出的任务，则返回当前时间，否则返回空
     pub fn is_require_pop(&self) -> Option<u64> {
-        let current_time = (minstant::now() as f64 * minstant::nanos_per_cycle() / 1000000.0).trunc() as u64;
+        let current_time = minstant::Instant::now().elapsed().as_millis() as u64;
         if self.timer.borrow().check_sleep(current_time) == 0 {
             Some(current_time)
         } else {
