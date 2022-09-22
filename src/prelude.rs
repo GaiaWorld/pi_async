24fc0781fb04f382825a79aaf8232f65ca742c75
zuon <zuon@yinengyun.com>
2022-09-22 16:33:59 +0800

    Remove default feature

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,13 +3,13 @@ pub use crate::lock::mutex_lock::*;
 pub use crate::lock::rw_lock::*;
 pub use crate::rt::async_pipeline::*;
 
-#[cfg(any(feature = "default"))]
+#[cfg(not(feature = "serial"))]
 pub use crate::rt::*;
-#[cfg(any(feature = "default"))]
+#[cfg(not(feature = "serial"))]
 pub use crate::rt::single_thread::*;
-#[cfg(any(feature = "default"))]
+#[cfg(not(feature = "serial"))]
 pub use crate::rt::multi_thread::*;
-#[cfg(any(feature = "default"))]
+#[cfg(not(feature = "serial"))]
 pub use crate::rt::worker_thread::*;
 
 #[cfg(feature = "serial")]
